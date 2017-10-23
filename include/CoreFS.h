adde4a59cdcf5dd109899eaf1c412a288386dc2d
n2omatt <n2omatt@amazingcow.com>
2017-10-23 04:42:20 -0200

    - Add the notice to the license header.

--- include/CoreFS.h
+++ include/CoreFS.h
@@ -15,6 +15,43 @@
 //  Copyright : AmazingCow - 2017                                             //
 //                                                                            //
 //  Description :                                                             //
+//    This lib is a "rip-off" of the C# Environment class and                 //
+//    python's os.path module.                                                //
+//                                                                            //
+//    We find very useful have those "functions" on C# and python,            //
+//    but we want to use them on C++ as well.                                 //
+//    We made a lot of effort to make it correct and respect the              //
+//    semantics of both original API and C++ usage.                           //
+//    But there some places that it wasn't possible or would make             //
+//    the API hard to use. On these places we choose to the possibility       //
+//    of the easier usage.                                                    //
+//                                                                            //
+//    There some functions that aren't implemented and we don't have          //
+//    plans to implement in a near future, for those, you're free             //
+//    to implement ;D                                                         //
+//                                                                            //
+//    On the C# API we tried to match as much as possible the usage           //
+//    of the Windows SDK, there are SEVERAL values for                        //
+//    CoreFS::SpecialFolder that aren't defined for Unix-like OSes            //
+//    Just like it's on the .net side too.                                    //
+//                                                                            //
+//    Today we're matching the Mono implementation of those values,           //
+//    but maybe if we find a easier and correct way, we might add "hacks"     //
+//    for values that aren't defined in mono.                                 //
+//                                                                            //
+//    Lastly, our target here is make it very usable both on Windows and      //
+//    GNU/Linux,there are no efforts currently to make it usable on OSX       //
+//    or other Unices neither for mobile platforms.                           //
+//    But this might change on future.                                        //
+//                                                                            //
+//    Have fun...                                                             //
+//    n2omatt - <n2omatt@amazingcow.com>                                      //
+//                                                                            //
+//    C# API Reference:                                                       //
+//      https://msdn.microsoft.com/en-us/library/system.environment.aspx      //
+//    Python API Reference:                                                   //
+//       https://docs.python.org/2/library/os.path.html                       //
+//                                                                            //
 //---------------------------------------------------------------------------~//
 
 #pragma once
@@ -23,44 +60,6 @@
 #include <vector>
 //CoreFS
 #include "CoreFS_Utils.h"
-
-//
-// Notice:
-//   This lib is a "rip-off" of the C# Environment class and
-//   python's os.path module.
-//
-//   We find very useful have those "functions" on C# and python,
-//   but we want to use them on C++ as well.
-//   We made a lot of effort to make it correct and respect the
-//   semantics of both original API and C++ usage.
-//   But there some places that it wasn't possible or would make
-//   the API hard to use. On these places we choose to the possibility
-//   of the easier usage.
-//
-//   There some functions that aren't implemented and we don't have
-//   plans to implement in a near future, for those, you're free
-//   to implement ;D
-//
-//   On the C# API we tried to match as much as possible the usage
-//   of the Windows SDK, there're SEVERAL values for CoreFS::SpecialFolder
-//   that aren't defined for Unix-like OSes - Just like it's on the .net
-//   side too.
-//   Today we're matching the Mono implementation of those values,
-//   but maybe if we find a easier and correct way, we might add "hacks"
-//   for values that aren't defined in mono.
-//
-//   Lastly, our target here is make it very usable both on Windows and GNU/Linux,
-//   there are no efforts currently to make it usable on OSX or other Unices neither
-//   for mobile platforms. But this might change on future.
-//
-//   Have fun...
-//   n2omatt - <n2omatt@amazingcow.com>
-//
-//   C# API Reference:
-//      https://msdn.microsoft.com/en-us/library/system.environment(v=vs.110).aspx
-//   Python API Reference:
-//      https://docs.python.org/2/library/os.path.html
-//
 
 NS_COREFS_BEGIN
 
